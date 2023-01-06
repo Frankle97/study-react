d798a806ab7049c35f4b112ea0849715ddd42ab3
Frankle97 <pairpapa@gmail.com>
2023-01-07 00:27:09 +0900

    Migrate react-todo-app

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./App.css";
+import Lists from "./components/Lists";
+import Form from "./components/Form";
+
+export default function App() {
+  const [todoData, setTodoData] = useState([]);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    // 이벤트 발생 시 페이지 리로드를 막아줌
+    e.preventDefault();
+
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: value,
+      completed: false,
+    };
+
+    // 할 일 덮어쓰기
+    setTodoData((prev) => [...prev, newTodo]);
+    setValue("");
+  };
+
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>할 일 목록</h1>
+        </div>
+        <Lists todoData={todoData} setTodoData={setTodoData} />
+        <Form
+          handleSubmit={handleSubmit}
+          value={value}
+          setValue={setValue}
+        ></Form>
+      </div>
+    </div>
+  );
+}
