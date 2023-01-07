03534c1e2d873c25d4ee2bae94fd374b3906c42c
Frankle97 <pairpapa@gmail.com>
2023-01-07 20:31:37 +0900

    import axios

---
+++ react-netflix-clone/src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.rog/3",
+    params: {
+        api_key: "6119607661ec7eeff4875532bd595b37",
+        language: "ko-KR"
+    }
+});
+
+export default instance;
