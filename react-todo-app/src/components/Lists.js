c4d036c8322b1795062a7ad7cc82886146670ee4
Frankle97 <pairpapa@gmail.com>
2023-01-07 15:02:40 +0900

    feat: 수정 기능 추가

--- react-todo-app/src/components/Lists.js
+++ react-todo-app/src/components/Lists.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 const Lists = React.memo(({ todoData, setTodoData }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(todoData.title);
+
   const handleCompleteChange = (id) => {
     let newTodo = todoData.map((data) => {
       if (data.id === id) {
@@ -31,57 +34,68 @@ const Lists = React.memo(({ todoData, setTodoData }) => {
     setTodoData(newTodoData);
   };
 
-  return (
-    <DragDropContext onDragEnd={handleEnd}>
-      <Droppable droppableId="todo">
-        {(provided) => (
-          <div {...provided.droppableProps} ref={provided.innerRef}>
-            {todoData.map((data, index) => (
-              <Draggable
-                key={data.id}
-                draggableId={data.id.toString()}
-                index={index}
-              >
-                {(provided, snapshot) => (
-                  <div
-                    key={data.id}
-                    {...provided.draggableProps}
-                    ref={provided.innerRef}
-                    {...provided.dragHandleProps}
-                    className={`${
-                      snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                    } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
-                  >
-                    <div className="items-center">
-                      <input
-                        type="checkbox"
-                        onClick={() => handleCompleteChange(data.id)}
-                        defaultChecked={data.completed}
-                      />
-                      <span
-                        className={data.completed ? "line-through" : undefined}
+  if (isEditing) {
+    return <div>editing...</div>
+  } else {
+    return (
+        <DragDropContext onDragEnd={handleEnd}>
+          <Droppable droppableId="todo">
+            {(provided) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {todoData.map((data, index) => (
+                      <Draggable
+                          key={data.id}
+                          draggableId={data.id.toString()}
+                          index={index}
                       >
+                        {(provided, snapshot) => (
+                            <div
+                                key={data.id}
+                                {...provided.draggableProps}
+                                ref={provided.innerRef}
+                                {...provided.dragHandleProps}
+                                className={`${
+                                    snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+                                } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
+                            >
+                              <div className="items-center">
+                                <input
+                                    type="checkbox"
+                                    onClick={() => handleCompleteChange(data.id)}
+                                    defaultChecked={data.completed}
+                                />
+                                <span
+                                    className={data.completed ? "line-through" : undefined}
+                                >
                         {data.title}
                       </span>
-                    </div>
-                    <div className="items-center">
-                      <button
-                        className="px-4 py-2 float-right"
-                        onClick={() => handleClick(data.id)}
-                      >
-                        x
-                      </button>
-                    </div>
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
+                              </div>
+                              <div className="items-center">
+                                <button
+                                    className="px-4 py-2 float-right"
+                                    onClick={() => handleClick(data.id)}
+                                >
+                                  x
+                                </button>
+                                <button
+                                    className="px-4 py-2 float-right"
+                                    onClick={() => setIsEditing(true)}
+                                >
+                                  edit
+                                </button>
+                              </div>
+                            </div>
+                        )}
+                      </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+    );
+
+  }
 });
 
 export default Lists
