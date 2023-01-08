f4ab62493e9a817d403d92b850bbbcf74f91d009
Frankle97 <pairpapa@gmail.com>
2023-01-08 13:10:20 +0900

    feat: Add MovieModal Component

--- react-netflix-clone/src/components/Row.js
+++ react-netflix-clone/src/components/Row.js
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import axios from "../api/axios";
 import "./Row.css";
+import MovieModal from "./MovieModal";
 
-export default function Row({ isLargeLow, title, id, fetchUrl }) {
-  const [movies, setMovies]  = useState([]);
-  
+export default function Row({isLargeLow, title, id, fetchUrl}) {
+  const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
+
   useEffect(() => {
     fetchMovieData().then();
   }, []);
@@ -13,38 +16,47 @@ export default function Row({ isLargeLow, title, id, fetchUrl }) {
   const fetchMovieData = async () => {
     const request = await axios.get(fetchUrl);
     setMovies(request.data.results);
-  }
+  };
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
 
   return (<section className="row">
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
           <span className="arrow"
-          onClick={() => {
-          document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-          }
-          }>{"<"}</span>
+                onClick={() => {
+                  document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+                }
+                }>{"<"}</span>
         </div>
         <div id={id} className="row__posters">
           {movies.map((movie) => (
             <img
               key={movie.id}
               className={`row__poster ${isLargeLow && "row__posterLarge"}`}
-              src={`https://image.tmdb.org/t/p/original/${isLargeLow ? movie.poster_path: movie.backdrop_path}`}
+              src={`https://image.tmdb.org/t/p/original/${isLargeLow ? movie.poster_path : movie.backdrop_path}`}
               alt={movie.name}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
         <div className="slider__arrow-right">
           <span className="arrow"
-          onClick={() => {
-          document.getElementById(id).scrollLeft += window.innerWidth += 80;
-          }
-          }
+                onClick={() => {
+                  document.getElementById(id).scrollLeft += window.innerWidth += 80;
+                }
+                }
           >{">"}</span>
         </div>
-
       </div>
+
+      {
+        modalOpen && (<MovieModal {...movieSelected} setModalOpen={setModalOpen}/>)
+      }
     </section>
   );
 }
