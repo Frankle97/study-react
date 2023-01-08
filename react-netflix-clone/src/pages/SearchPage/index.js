b7d142d04bb4723fc7c804d2022a82eb0a592c82
Frankle97 <pairpapa@gmail.com>
2023-01-08 19:41:32 +0900

    feat: Search UI & 기능 구현

--- react-netflix-clone/src/pages/SearchPage/index.js
+++ react-netflix-clone/src/pages/SearchPage/index.js
@@ -1,8 +1,61 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useLocation} from "react-router-dom";
+import axios from "../../api/axios";
+import "./SearchPage.css";
 
 export default function SearchPage() {
-  return (
-    <div></div>
-  );
+  const [searchResult, setSearchResult] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm).then();
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+      setSearchResult(request.data.results);
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+    const renderSearchResults = () => {
+      return searchResult.length > 0 ? (
+        <section className="search-container">
+          {searchResult.map((movie) => {
+            if (movie.backdrop_path !== null && movie.media_type !== "person") {
+              const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+              return(
+                <div className="movie" key={movie.id}>
+                  <div className="movie__column-poster">
+                    <img
+                      src={movieImageUrl} alt="movie"
+                    className="movie__poster"/>
+                  </div>
+                </div>
+              )
+          }
+          })}
+        </section>
+      ) : (
+        <section className="no-results">
+          <div className="no-results__text">
+            <p>
+              찾고자하는 검색어"{searchTerm}"에 맞는 영화가 없습니다.
+            </p>
+          </div>
+        </section>
+      )
+    };
+
+  return renderSearchResults();
 }
 
