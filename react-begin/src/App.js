4f49d8073cc0f59844f1c778ee6a75662001dd18
Frankle97 <pairpapa@gmail.com>
2023-01-20 11:15:45 +0900

    feat: useReducer 적용
    useState와 마찬가지로 상태를 관리하는데, useStaet와 달리 함수 외부에서도, 파일 바깥에서도 상태를 관리할 수 있음

--- react-begin/src/App.js
+++ react-begin/src/App.js
@@ -1,17 +1,17 @@
 import UserList from "./UserList";
-import {useCallback, useMemo, useRef, useState} from "react";
+import {useCallback, useMemo, useReducer, useRef, useState} from "react";
 import CreateUser from "./CreateUser";
 
 function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: '',
     email: ''
-  });
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       username: 'velopert',
@@ -30,65 +30,95 @@ function App() {
       email: 'liz@example.com',
       active: false
     }
-  ]);
+  ]
+}
 
-  const {username, email} = inputs;
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.id ? { ...user, active: !user.active } : user)
+      }
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      }
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const nextId = useRef(4); // 리렌더링에도 초기화되지 않음
+
+  const {users} = state;
+  const {username, email} = state.inputs;
+
   const onChange = useCallback(e => {
-      const {name, value} = e.target;
-      /**
-       * 기존 inputs 는 옮겨 담고 새로운 value 는 name 과 매칭하여 값 세팅
-       */
-      setInputs(inputs => ({
-        ...inputs,
-        [name]: value
-      }));
-    }, []);
-
-  /**
-   * useRef() 는 re-rendering 시 초기화되지 않는다.
-   */
-  const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setUsers(users => [...users, user]); // 불변을 유지하기 위해 spread operator 사용하여 기존 배열을 복사
-
-    setInputs({
-      username: '',
-      email: ''
+    const {name, value} = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
     });
-    nextId.current += 1;
-  }, [username, email]);
-
-  const onRemove = useCallback(id => {
-    setUsers(users => users.filter(user => user.id !== id));
-  }, [users]);
-
-  const onToggle = useCallback(id => {
-    setUsers(users =>
-      users.map(user =>
-        user.id === id ? {...user, active: !user.active} : user
-      )
-    );
   }, []);
 
+  const onCreate = useCallback(() => {
+      dispatch({
+        type: 'CREATE_USER',
+        user: {
+          id: nextId.current,
+          username,
+          email
+        }
+      });
+      nextId.current += 1;
+    },
+    [username, email]);
+
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  }, [])
+
+  const onRemove = useCallback(id => {
+      dispatch({
+        type: 'REMOVE_USER',
+        id
+      });
+    }, [],
+  );
 
   const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
-      <CreateUser
-        username={username}
-        email={email}
-        onCreate={onCreate}
-        onChange={onChange}
+      <CreateUser username={username}
+                  email={email}
+                  onChange={onChange}
+                  onCreate={onCreate}
       />
       <UserList users={users}
-                onRemove={onRemove}
                 onToggle={onToggle}
-      />
+                onRemove={onRemove}/>
       <div>활성 사용자 수: {count}</div>
     </>
   );
