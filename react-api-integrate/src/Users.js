abb755d1dcfc91a65eadfa4217a8f3e4e602052a
Frankle97 <pairpapa@gmail.com>
2023-02-01 10:36:43 +0900

    react-useAsync 적용

--- react-api-integrate/src/Users.js
+++ react-api-integrate/src/Users.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useReducer, useState} from 'react';
 import axios from "axios";
-import useAsync from "./useAsync";
 import User from "./User";
+import {useAsync} from "react-async";
 
 async function getUsers() {
   const response = await axios.get(
@@ -12,13 +12,13 @@ async function getUsers() {
 
 function Users() {
   const [userId, setUserId] = useState(null);
-  const [state, refetch] = useAsync(getUsers, [], true);
+  const {data: users, error, isLoading, reload} = useAsync({
+    promiseFn: getUsers,
+  });
 
-  const {loading, data: users, error} = state;
-
-  if (loading) return <div>로딩중...</div>;
+  if (isLoading) return <div>로딩중...</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return <button onClick={refetch}>불러오기</button>;
+  if (!users) return <button onClick={reload}>불러오기</button>;
   return (
     <div>
       <ul>
@@ -32,7 +32,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={refetch}>다시 불러오기</button>
+      <button onClick={reload}>다시 불러오기</button>
       {userId && <User id={userId}/>}
     </div>
   );
