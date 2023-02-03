e9ca83143f454a591458302dd1e72eeb6f9606df
Frankle97 <pairpapa@gmail.com>
2023-02-03 16:17:22 +0900

    TypeScript Function Practice

--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -1,20 +1,9 @@
-let count = 0;
-count += 1;
-// count = '갑자기 분위기 문자열'; [X]
+function sum(x: number, y: number): number {
+  return x + y;
+}
 
-const message: string = 'hello world';
+sum(1, 2);
 
-const done: boolean = true;
-
-const numbers: number[] = [1, 2, 3];
-const messages: string[] = ['hello', 'world'];
-
-// messages.push(1); // message type is string. cannot input number [X]
-
-let mightBeUndefined: string | undefined = undefined; // string or undefined
-let nullableNumber: number | null = null;
-
-
-let color: 'red' | 'orange' | 'yellow' = 'red';
-color = 'yellow';
-// color = 'green'; [X] red, orange, yellow 만 입력 가능
+function sumArray(numbers: number[]): number {
+  return numbers.reduce((acc, current) => acc + current, 0);
+}
