80b3f6cee393494d79a3512a5a69f82dd2361827
Frankle97 <pairpapa@gmail.com>
2023-02-04 18:01:22 +0900

    TS-React useRouter

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import MyForm from "./MyForm";
+import ReducerSample from "./ReducerSample";
 
 const App: React.FC = () => {
   const onSubmit = (form: { name: string; description: string }) => {
     console.log(form);
   };
 
-  return <MyForm onSubmit={onSubmit}/>;
+  return <ReducerSample/>;
 }
 
 export default App;
