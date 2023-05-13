36040b1d8edd8e33a882b978f2e9b3f074508d43
Frankle97 <pairpapa@gmail.com>
2023-05-13 10:36:17 +0900

    use redux-actions

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,34 +1,23 @@
 // 액션 타입 정의
+import {createAction, handleAction, handleActions} from "redux-actions";
+
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 // 액션 생성 함수 정의
-export const increase = () => ({
-  type: INCREASE
-});
-export const decrease = () => ({
-  type: DECREASE
-});
-
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기 상태 정의
 const initialState = {
   number: 0
 };
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1
-      };
-    default:
-      return state;
-  }
-};
+const counter = handleActions({
+    [INCREASE]: (state, action) => ({number: state.number + 1}),
+    [DECREASE]: (state, action) => ({number: state.number - 1})
+  },
+  initialState
+);
 
 export default counter;
