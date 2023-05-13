57d92b282f3cb75abd062f75c8a1ba7285774fe6
Frankle97 <pairpapa@gmail.com>
2023-05-13 10:59:54 +0900

    use useActions
    - 여러 개의 액션을 사용해야 하는 경우 코드를 간소화할 수 있음
    - 변환: 액션 생성 함수 => 액션을 디스패치하는 함수

--- react-redux-tutorial/src/components/TodosContainer.js
+++ react-redux-tutorial/src/components/TodosContainer.js
@@ -2,17 +2,23 @@ import Todos from "./Todos";
 import {changeInput, insert, remove, toggle} from "../modules/todos";
 import {connect, useDispatch, useSelector} from "react-redux";
 import {useCallback} from "react";
+import useActions from "../lib/useActions";
 
 const TodosContainer = () => {
   const {input, todos} = useSelector(({ todos }) => ({
     input: todos.input,
     todos: todos.todos
   }));
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
-  const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+
+  // const dispatch = useDispatch();
+  // const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
+  // const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
 
   return (
     <Todos
