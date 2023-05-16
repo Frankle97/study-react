b141fa462b262f4cd3a0a34d966d57b33283c807
Frankle97 <pairpapa@gmail.com>
2023-05-16 12:10:31 +0900

    Sample redux-saga 적용

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,15 +1,59 @@
 import * as api from "../lib/api";
-import {handleActions} from "redux-actions";
-import createRequestThunk from "../lib/createRequestThunk";
+import {createAction, handleActions} from "redux-actions";
+import {finishLoading, startLoading} from "./loading";
+import {call, put, takeLatest} from 'redux-saga/effects';
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
+const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
+const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getPost = createAction(GET_POST, id => id);
+export const getUsers = createAction(GET_USERS);
+
+function* getPostSaga(action) {
+  yield put(startLoading(GET_POST));
+  try {
+    const post = yield call(api.getPost, action.payload);
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post.data
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true
+    });
+  }
+  yield put(finishLoading(GET_POST));
+}
+
+function* getUsersSaga() {
+  yield put(startLoading(GET_USERS));
+  const users = yield call(api.getUsers);
+  try {
+    yield put({
+      type: GET_USERS_SUCCESS,
+      payload: users.data
+    });
+  } catch (e) {
+    yield put({
+      type: GET_USERS_FAILURE,
+      payload: e,
+      error: true
+    });
+  }
+  yield put(finishLoading(GET_USERS));
+}
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
 
 const initialState = {
   post: null,
